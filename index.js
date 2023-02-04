20eb973ba1e1b05396de4b24dc409f02e75b3b29
Juan Sanchez <juansan1993@hotmail.com>
2023-02-04 18:13:30 -0500

    Everything done!

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 /*Scores*/
 let manScore = 0;
 let computerScore = 0;
-
 /*HTML elements*/
 const rock = document.getElementById('rock');
 const paper = document.getElementById('paper');
 const scissors = document.getElementById('scissors');
 const manScoreText = document.querySelector('.manScore');
 const computerScoreText = document.querySelector('.computerScore');
-
+const manResult = document.querySelector('#man-result');
+const computerResult = document.querySelector('#computer-result');
+const body = document.querySelector('body');
 /*Helper function to get the computers answer*/
 function getComputerChoice() {
   if (Math.ceil(Math.random() * 3) === 1) {
@@ -19,11 +20,9 @@ function getComputerChoice() {
     return 'Scissors';
   }
 }
-
 /*Checks who wins the round*/
 function playRound(playerSelection, computerSelection) {
   console.log(playerSelection, computerSelection);
-
   if (playerSelection === 'Rock' && computerSelection === 'Paper') {
     computerScore += 1;
   } else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
@@ -37,41 +36,52 @@ function playRound(playerSelection, computerSelection) {
   } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
     manScore += 1;
   } else if (playerSelection === computerSelection) {
-    // return 'Draw';
   }
 }
 
 /*Choices values*/
-// let playerSelection = 'Rock';
 let playerSelection = undefined;
+let computerSelection = getComputerChoice();
 
 rock.addEventListener('click', () => {
   playerSelection = 'Rock';
-  getComputerChoice();
+  computerSelection = getComputerChoice();
   game();
+  manResult.innerText = playerSelection;
+  computerResult.innerText = computerSelection;
+  gameOver();
 });
 paper.addEventListener('click', () => {
   playerSelection = 'Paper';
-  getComputerChoice();
+  computerSelection = getComputerChoice();
   game();
+  manResult.innerText = playerSelection;
+  computerResult.innerText = computerSelection;
+  gameOver();
 });
 scissors.addEventListener('click', () => {
   playerSelection = 'Scissors';
-  getComputerChoice();
+  computerSelection = getComputerChoice();
   game();
+  manResult.innerText = playerSelection;
+  computerResult.innerText = computerSelection;
+  gameOver();
 });
-
-let computerSelection = getComputerChoice();
-
-// console.log(playerSelection, computerSelection);
 
 function game() {
   playRound(playerSelection, computerSelection);
-  // console.log('man ' + manScore);
   manScoreText.innerText = `${manScore}`;
-  // console.log('computer ' + computerScore);
   computerScoreText.innerText = `${computerScore}`;
 }
 
-let result = game();
-console.log(result);
+function gameOver() {
+  if (manScore === 10) {
+    body.innerText = 'You won!!!!!!!!!!!';
+    body.style.lineHeight = '500px';
+    body.style.fontSize = '100px';
+  } else if (computerScore === 10) {
+    body.innerText = 'You lose :c';
+    body.style.lineHeight = '500px';
+    body.style.fontSize = '100px';
+  }
+}
