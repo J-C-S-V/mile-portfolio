39b567c179a2e5cfd72ac68c71c7322cdf86d0cc
Juan Sanchez <juansan1993@hotmail.com>
2023-02-03 22:31:21 -0500

    Making functions

---
+++ index.js
@@ -0,0 +1,12 @@
+function playRound(playerSelection, computerSelection) {
+  // your code here!
+}
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+  playRound();
+  return;
+}
